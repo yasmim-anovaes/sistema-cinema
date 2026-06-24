68a6d849c46089d492971753b11fef245f471f10
LORENZO ANDRADE GONCALVES <lorenzo.agoncalves@senacsp.edu.br>
2026-06-24 16:41:23 -0300

    Tirando o local da conexão.

--- javascript/conexao/conexao.js
+++ javascript/conexao/conexao.js
@@ -7,7 +7,7 @@ console.log("DB_NAME:", process.env.DB_NAME);
 
 const conexao = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
    

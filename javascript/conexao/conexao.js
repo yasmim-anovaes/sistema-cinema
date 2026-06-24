0657c6acf5be4557b18f74324119151612c093da
LORENZO ANDRADE GONCALVES <lorenzo.agoncalves@senacsp.edu.br>
2026-06-24 16:42:57 -0300

    Arrumando a classe de conexão.

--- javascript/conexao/conexao.js
+++ javascript/conexao/conexao.js
@@ -8,6 +8,7 @@ console.log("DB_NAME:", process.env.DB_NAME);
 const conexao = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
    
